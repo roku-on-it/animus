5b69182f65c3597252e6eef0098ed86060e38f4c
wilyfwonka <tyserract@gmail.com>
2022-01-07 02:12:41 +0300

    refactor: rate limit by user id or ip address

--- src/module/auth/guard/gql-throttler.guard.ts
+++ src/module/auth/guard/gql-throttler.guard.ts
@@ -4,9 +4,17 @@ import { GqlExecutionContext, GraphQLArgumentsHost } from '@nestjs/graphql';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
+  // Overriding the default error message.
+  errorMessage = 'Too many requests';
+
   getRequestResponse(context: ExecutionContext) {
     const gqlCtx: GraphQLArgumentsHost = GqlExecutionContext.create(context);
     const ctx = gqlCtx.getContext();
+
+    // Overriding the generateKey function to prevent generating md5 hash by default.
+    this.generateKey = () => {
+      return ctx.req.session.userId || ctx.req.ip;
+    };
 
     return { req: ctx.req, res: ctx.res };
   }

f2c7a227833b520e3ce732a35f63223f57ad43d7
Oguz Turkay <tyserract@gmail.com>
2022-04-24 21:57:54 +0300

    delete redundant test files

--- src/module/user/input/list-user.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ListUser } from './list-user';
-
-describe('ListUser', () => {
-  it('should be defined', () => {
-    expect(new ListUser()).toBeDefined();
-  });
-});
--- src/module/user/input/update-me.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateMe } from './update-me';
-
-describe('UpdateMe', () => {
-  it('should be defined', () => {
-    expect(new UpdateMe()).toBeDefined();
-  });
-});

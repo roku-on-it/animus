24b0b2659aae5b9984eaf20a6644235540eed512
Oguz Turkay <tyserract@gmail.com>
2022-04-25 06:18:56 +0300

    refactor deprecated plainToClass

--- src/module/auth/service/consumer/register.consumer.ts
+++ src/module/auth/service/consumer/register.consumer.ts
@@ -2,7 +2,7 @@ import { Process, Processor } from '@nestjs/bull';
 import { QueueType } from 'src/module/misc/app-queue/type/queue-type.enum';
 import { Job } from 'bull';
 import { JobType } from 'src/module/misc/app-queue/type/job-type.num';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { User } from 'src/module/user/model/user';
 import { CreateUser } from 'src/module/user/input/create-user';
 
@@ -10,6 +10,6 @@ import { CreateUser } from 'src/module/user/input/create-user';
 export class RegisterConsumer {
   @Process(JobType.REGISTER)
   async handleRegisterJob(job: Job<CreateUser>) {
-    return plainToClass(User, job.data).save();
+    return plainToInstance(User, job.data).save();
   }
 }

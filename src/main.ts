d016e87b33eb25e23f4ae7fb46abd727e99194ba
Oguz Turkay <tyserract@gmail.com>
2022-04-26 08:25:58 +0300

    just hide powered by header

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
+  app.use(helmet.hidePoweredBy());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
 

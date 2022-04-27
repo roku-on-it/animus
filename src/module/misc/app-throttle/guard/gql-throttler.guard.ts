01a50caa2575104f2ae45d047ee0dfe25d76843b
Oguz Turkay <tyserract@gmail.com>
2022-04-28 01:36:11 +0300

    refactor: get ttl from redis directly & add true-blue privilege

--- src/module/misc/app-throttle/guard/gql-throttler.guard.ts
+++ src/module/misc/app-throttle/guard/gql-throttler.guard.ts
@@ -1,22 +1,44 @@
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { ThrottlerGuard } from '@nestjs/throttler';
-import { GqlExecutionContext, GraphQLArgumentsHost } from '@nestjs/graphql';
+import { GqlExecutionContext } from '@nestjs/graphql';
+import { TrueBlue } from '../../../true-blue/model/true-blue';
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
   // Overriding the default error message.
   errorMessage = 'Too many requests';
 
-  getRequestResponse(context: ExecutionContext) {
-    const gqlCtx: GraphQLArgumentsHost = GqlExecutionContext.create(context);
-    const ctx = gqlCtx.getContext();
+  protected async handleRequest(context: ExecutionContext, limit, ttl) {
+    const ctx = GqlExecutionContext.create(context).getContext();
+    const identifier = ctx.req.session.userId;
 
-    // Overriding the generateKey function to prevent generating md5 hash by default.
-    this.generateKey = () => {
-      const handlerName = context.getHandler().name;
-      return handlerName + (ctx.req.session.userId ?? ctx.req.ip);
-    };
+    if (null != identifier) {
+      const trueBlue = await TrueBlue.findOne({
+        where: {
+          user: identifier,
+        },
+        select: ['id'],
+      });
 
-    return { req: ctx.req, res: ctx.res };
+      if (null != trueBlue) {
+        return true;
+      }
+    }
+
+    const key = context.getHandler().name + (identifier ?? ctx.req.ip);
+    const ttls = await this.storageService.getRecord(key);
+    const nearestExpiryTime = 0 !== ttls.length ? Math.min(...ttls) : ttl;
+
+    if (ttls.length >= limit) {
+      ctx.res.header('X-Retry-After', nearestExpiryTime);
+      this.throwThrottlingException(context);
+    }
+
+    ctx.res.header(this.headerPrefix + '-Remaining', limit - ttls.length - 1);
+    ctx.res.header(this.headerPrefix + '-Reset', nearestExpiryTime);
+
+    await this.storageService.addRecord(key, ttl);
+
+    return true;
   }
 }

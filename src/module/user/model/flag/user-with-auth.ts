57597f74bb90d43d5ed8c76979d8c56b4fcd92f0
Oguz Turkay <tyserract@gmail.com>
2022-04-28 07:03:02 +0300

    refactor: add typing

--- src/module/user/model/flag/user-with-auth.ts
+++ src/module/user/model/flag/user-with-auth.ts
@@ -3,7 +3,7 @@ import { FluentFlag } from '../../../shared/interface/fluent-flag';
 import { User } from '../user';
 
 export class UserWithAuth implements FluentFlag {
-  static findOneOrFail(...args) {
+  static findOneOrFail(...args): Promise<User> {
     return User.findOneOrFail(...args).catch((error) => {
       if (error instanceof NotFoundException) {
         throw new UnauthorizedException();

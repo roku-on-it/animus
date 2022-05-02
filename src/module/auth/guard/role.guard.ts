d54de111454e798667945f630b0da42b132d5f1b
Oguz Turkay <tyserract@gmail.com>
2022-05-02 04:52:54 +0300

    add true blue check for role guard

--- src/module/auth/guard/role.guard.ts
+++ src/module/auth/guard/role.guard.ts
@@ -1,4 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { UserRole } from 'src/module/user/model/enum/user-role';
@@ -22,10 +27,15 @@ export class RoleGuard implements CanActivate {
       return false;
     }
 
-    const user = await User.withAuth.findOneOrFail(
-      { id: session.userId },
-      { select: ['role'] },
-    );
+    const user = await User.createQueryBuilder('user')
+      .where({ id: session.userId })
+      .select(['user.role', 'trueBlue.hasTrueRootPrivilege'])
+      .leftJoin('user.trueBlue', 'trueBlue') // bar is the joined table
+      .getOne();
+
+    if (null == user) {
+      throw new UnauthorizedException();
+    }
 
     return user.role >= role;
   }

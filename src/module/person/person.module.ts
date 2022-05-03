46291695fdb7f4143cba2faad0ca41b9a96c7fbe
Oguz Turkay <tyserract@gmail.com>
2022-05-03 04:08:00 +0300

    create person module

--- src/module/person/person.module.ts
+++ src/module/person/person.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PersonResolver } from './person.resolver';
 
-@Module({})
+@Module({
+  providers: [PersonResolver],
+})
 export class PersonModule {}

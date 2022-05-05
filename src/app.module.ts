c88435c1dd9893ba9cc84c29f8912bed90c8b133
Oguz Turkay <tyserract@gmail.com>
2022-05-05 06:38:01 +0300

    add migration script on startup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppConfigModule } from 'src/module/misc/app-config/app-config.module';
 import { AppTypeormModule } from 'src/module/misc/app-typeorm/app-typeorm.module';
 import { AppGraphqlModule } from 'src/module/misc/app-graphql/app-graphql.module';
@@ -7,6 +7,7 @@ import { AppThrottleModule } from 'src/module/misc/app-throttle/app-throttle.mod
 import { UserModule } from './module/user/user.module';
 import { TrueBlueModule } from './module/true-blue/true-blue.module';
 import { PersonModule } from './module/person/person.module';
+import { getManager } from 'typeorm';
 
 @Module({
   imports: [
@@ -20,4 +21,15 @@ import { PersonModule } from './module/person/person.module';
     PersonModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  async onModuleInit(): Promise<void> {
+    await getManager().query(`
+    ALTER TABLE person_acquaintances_person
+    DROP CONSTRAINT IF EXISTS cannot_know_self;
+    
+    ALTER TABLE person_acquaintances_person
+    ADD CONSTRAINT cannot_know_self
+    CHECK ("personId_1" <> "personId_2");
+     `);
+  }
+}

1c451ca5be629d1c01a64ee8f1e298e41865be99
Oguz Turkay <tyserract@gmail.com>
2022-05-12 14:08:19 +0300

    temporarily add validations

--- src/module/identity/input/update-identity.ts
+++ src/module/identity/input/update-identity.ts
@@ -1,24 +1,28 @@
 import { InputType } from '@nestjs/graphql';
 import { UpdateModel } from '../../shared/input/update-model';
 import { Trim } from '../../shared/decorator/transform/trim';
-import { IsString } from 'class-validator';
+import { MinLength } from 'class-validator';
 import { OptionalField } from '../../shared/decorator/property/optional-field';
 
 @InputType()
 export class UpdateIdentity extends UpdateModel {
   @OptionalField()
   @Trim()
+  @MinLength(3)
   firstName: string;
 
   @OptionalField()
   @Trim()
+  @MinLength(3)
   lastName: string;
 
+  @OptionalField()
   @Trim()
-  @IsString()
+  @MinLength(3)
   nationalId: string;
 
-  @Trim()
   @OptionalField()
+  @Trim()
+  @MinLength(3)
   placeOfBirth: string;
 }

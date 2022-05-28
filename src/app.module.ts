24ed7890dcda1c3bdcf2937f69e0efa26dbdc631
Oguz Turkay <tyserract@gmail.com>
2022-05-28 18:44:10 +0300

    import last-known-place module

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,12 +12,19 @@ import { IdentityModule } from './module/identity/identity.module';
 import { AddressModule } from './module/address/address.module';
 import { NoteModule } from './module/note/note.module';
 import { ContactModule } from './module/contact/contact.module';
+import { LastKnownPlaceModule } from './module/last-known-place/last-known-place.module';
 
-// TODO: Specify the lengths of text based fields
-// TODO: Refactor @*Length & co-related decorators after specifying text length
-// TODO: Refactor @ResolveField decorated methods to return filter-*.find();
-// TODO: Add proper missing authorization & authentication to entity's resolver methods
-// TODO: Introduce setting optional object property with spread operator to list-*.ts classes ---> ...(condition && { key: "value" }),
+// TODO: Specify the lengths of text based fields.
+// TODO: Refactor @*Length & co-related decorators after specifying text length.
+// TODO: Refactor @ResolveField decorated methods to return filter-*.find();.
+// TODO: Add proper missing authorization & authentication to entity's resolver methods.
+// TODO: Introduce setting optional object property with spread operator to list-*.ts classes ---> ...(condition && { key: "value" }),.
+// TODO: Remove "person" "@Field()" from models as is it unnecessary.
+// TODO: Add "with" relation to LastKnownPlace.
+// TODO: Check for fields in create models. Look into create-last-known-place.ts file for more details.
+// TODO: Check if there is unnecessary @ResolveField() decorators on both Person and its child entities. Look into last-known-place.resolver.ts for more details.
+// TODO: Check if there is any non-nullable field but nullable on update operations. Look into update-last-known-place.ts for more details.
+// TODO: Try to turn `addAcquaintance` and `removeAcquaintance` into Person's "actions: {}" or "actions: []" rather than standalone mutations. Do the same for LastKnownPlace
 
 @Module({
   imports: [
@@ -34,6 +41,7 @@ import { ContactModule } from './module/contact/contact.module';
     AddressModule,
     NoteModule,
     ContactModule,
+    LastKnownPlaceModule,
   ],
 })
 export class AppModule {}

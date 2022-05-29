ec864f9e41698578937b9b8a9feb6b436b40dd0b
Oguz Turkay <tyserract@gmail.com>
2022-05-29 06:06:27 +0300

    add description for actions

---
+++ src/module/shared/constant/input-field-description.ts
@@ -0,0 +1,2 @@
+export const ACTIONS_DESCRIPTION =
+  'When multiple actions are defined in the `actions` body, the actions are executed in alphabetical order by key name. For example, if you had three actions named `foo`, `bar` and `baz` they would execute in the following order: `bar`&#8594;`baz`&#8594;`foo`';

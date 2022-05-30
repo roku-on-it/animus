199a02650d9e24d5d7bc70c4dd756be009b52c0f
Oguz Turkay <tyserract@gmail.com>
2022-05-30 03:46:26 +0300

    remove redundant person in person action input type

--- src/module/person/input/action/person-action.ts
+++ src/module/person/input/action/person-action.ts
@@ -1,11 +1,8 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { UpdateAcquaintance } from './acquaintance/update-acquaintance';
-import { Person } from '../../model/person';
 
 @InputType()
 export class PersonAction {
-  person: Person;
-
   @Field(() => UpdateAcquaintance, { nullable: true })
   addAcquaintance: UpdateAcquaintance;
 

2a20452c62c605e83829fbd845996bda22ad3be0
Oguz Turkay <tyserract@gmail.com>
2022-05-31 03:47:08 +0300

    add todo

--- src/module/person/input/action/person-action.ts
+++ src/module/person/input/action/person-action.ts
@@ -2,6 +2,9 @@ import { Field, InputType } from '@nestjs/graphql';
 import { AddAcquaintance } from './acquaintance/add-acquaintance';
 import { RemoveAcquaintance } from './acquaintance/remove-acquaintance';
 
+// Separating Add and Remove input types in order to distinguish the actions from each other
+// in case we want to handle them specifically one by one with "payload instance of AddAcquaintance".
+
 @InputType()
 export class PersonAction {
   @Field(() => AddAcquaintance, { nullable: true })

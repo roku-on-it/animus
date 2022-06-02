af34b793f949c8bea2b96a1e1cddca6ef6b779ec
Oguz Turkay <tyserract@gmail.com>
2022-06-02 09:04:30 +0300

    improve optional field to check for null explicitly

--- src/module/shared/decorator/property/optional-field.ts
+++ src/module/shared/decorator/property/optional-field.ts
@@ -1,17 +1,49 @@
 import { applyDecorators } from '@nestjs/common';
 import { Field, FieldOptions } from '@nestjs/graphql';
 import { ReturnTypeFunc } from '@nestjs/graphql/dist/interfaces/return-type-func.interface';
-import { IsOptional } from 'class-validator';
+import { IsOptional, ValidateIf } from 'class-validator';
 
-export const OptionalField = (
+interface OptionalFieldOptions extends FieldOptions {
+  explicitNullCheck?: boolean;
+}
+
+export function OptionalField(
+  options?: OptionalFieldOptions,
+): PropertyDecorator;
+export function OptionalField(
   returnTypeFunction?: ReturnTypeFunc,
-  options?: FieldOptions,
-): PropertyDecorator => {
+): PropertyDecorator;
+export function OptionalField(
+  returnTypeFunction?: ReturnTypeFunc,
+): PropertyDecorator;
+export function OptionalField(
+  returnTypeFunction?: ReturnTypeFunc,
+  options?: OptionalFieldOptions,
+): PropertyDecorator;
+export function OptionalField(
+  returnTypeFunction?: ReturnTypeFunc | OptionalFieldOptions,
+  options?: OptionalFieldOptions,
+): PropertyDecorator {
+  if (returnTypeFunction?.['explicitNullCheck']) {
+    if ('function' === typeof returnTypeFunction) {
+      return applyDecorators(
+        Field(returnTypeFunction, { nullable: true, ...returnTypeFunction }),
+        ValidateIf((target, value) => 'undefined' !== typeof value),
+      );
+    }
+
+    return applyDecorators(
+      Field({ nullable: true, ...returnTypeFunction }),
+      ValidateIf((target, value) => 'undefined' !== typeof value),
+    );
+  }
+
   if ('function' === typeof returnTypeFunction) {
     return applyDecorators(
       Field(returnTypeFunction, { nullable: true, ...options }),
       IsOptional(),
     );
   }
+
   return applyDecorators(Field({ nullable: true, ...options }), IsOptional());
-};
+}
